b7587bc114a816327f8b7d672dba195527002af9
atanu sarkar <atanu.sarkar@bosenet.com>
2023-08-30 18:24:09 +0530

    initial commit

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,20 @@ self.addEventListener('install',function(event){
         caches.open('static')
         .then(function(cache){
             console.log('[service worker] Precaching App Shell');
-            cache.add('/index.html')
+            // cache.add('/index.html')
+            cache.addAll([
+                '/',
+                '/index.html',
+                '/src/js/app.js',
+                '/src/js/feed.js',
+                '/src/js/material.min.js',
+                '/src/css/app.css',
+                '/src/css/feed.css',
+                '/src/images/main-image.jpg',
+                'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+                'https://fonts.googleapis.com/icon?family=Material+Icons'
+            ])
         })
         )
 });
@@ -21,7 +34,17 @@ self.addEventListener('fetch',function(event){
             if(response){
                 return response;
             }else{
-                return fetch(event.request);
+                return fetch(event.request)
+                    .then(function(res){
+                        return caches.open('dynamic')
+                            .then(function(cache){
+                                cache.put(event.request.url,res.clone())
+                                    return res;
+                            })
+                    })
+                    .catch(function(err){
+                        
+                    })
             }
         })        
     );
